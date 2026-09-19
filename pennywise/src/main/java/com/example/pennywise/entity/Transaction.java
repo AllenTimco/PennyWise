102f38d97dd3a346e50d41a4c4ea0bcf9aa6c84a
AlllenTimco <allenrtimco@gmail.com>
2026-09-19 13:37:12 -0400

    Transaction class

---
+++ pennywise/src/main/java/com/example/pennywise/entity/Transaction.java
@@ -0,0 +1,18 @@
+package com.example.pennywise.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Transaction 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    public Transaction(double Amount, String Groceries, String Catergory, String Date)
+    {
+
+    }
+}
